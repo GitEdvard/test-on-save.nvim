d82bff71fcd6a29c7f031e3d60a10ea98f2d9d78
Edvard Englund <edvard.englund@medsci.uu.se>
2022-12-29 10:55:07 +0100

    Implement python test suite

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -16,10 +16,23 @@ local scope_for_function = function(bufnr)
     return vim.inspect(text)
 end
 
+local scope_for_suite = function(bufnr)
+    local text = vim.fn.expand('%')
+    return vim.inspect(text)
+end
+
+
 
 vim.api.nvim_create_user_command("AttachTestMethod", function()
     local bufnr = vim.api.nvim_get_current_buf()
     local scope = scope_for_function(bufnr)
+    local command = "python -m pytest " .. scope .." 2>&1"
+    M.attach_test_range(bufnr, command, "*.py")
+end, {})
+
+vim.api.nvim_create_user_command("AttachTestClass", function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local scope = scope_for_suite(bufnr)
     local command = "python -m pytest " .. scope .." 2>&1"
     M.attach_test_range(bufnr, command, "*.py")
 end, {})
